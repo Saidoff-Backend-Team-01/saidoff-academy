77f935749af350134b067f4c123bb627d1152833
ShukuraliProgrammer <shukuralijob@gmail.com>
2024-09-13 20:26:59 +0500

    test-with data

--- app/admin/model_admins.py
+++ app/admin/model_admins.py
@@ -4,7 +4,7 @@ from app.models.banner import Banner, Why_we_us
 
 class BannerAdmin(ModelView, model=Banner):
     column_list = [Banner.id, Banner.title, Banner.desc]
-    form_columns = [Banner.id, Banner.title, Banner.desc]
+    form_columns = [Banner.id, Banner.title, Banner.desc, Banner.bg_image, Banner.phone_num]
     column_searchable_list = [Banner.title]
 
 class Why_we_usAdmin(ModelView, model=Why_we_us):
--- app/models/banner.py
+++ app/models/banner.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
+from fastapi_storages import FileSystemStorage
+from fastapi_storages.integrations.sqlalchemy import ImageType
 
 from app.config.database import Base
 
@@ -10,7 +12,7 @@ class Banner(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String, nullable=False)
     desc = Column(String, nullable=False)
-    bg_image = Column(String, nullable=False)
+    bg_image = Column(ImageType(storage=FileSystemStorage(path="/media/banner")))
     phone_num = Column(String(length=20), nullable=False)
 
 
