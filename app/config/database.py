26a29e36c69f918be00c48c09ea532ff37fb6cba
ShukuraliProgrammer <shukuralijob@gmail.com>
2024-09-13 21:02:38 +0500

    feat(test for banner list)

--- app/config/database.py
+++ app/config/database.py
@@ -1,14 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import os
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345sh_@localhost/saidoff_academy"
 
+SQLALCHEMY_DATABASE_URL_TESTING = "postgresql://postgres:12345sh_@localhost/saidoff_academy_test"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+if os.environ.get('ENVIRONMENT') == "testing":
+     engine = create_engine(
+     SQLALCHEMY_DATABASE_URL_TESTING,
+    )
+else:
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
